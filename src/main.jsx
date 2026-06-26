4eaed87e5bedbac17d92b33af3d9d427e2ec4618
Yorenin <168438614+Yorenin@users.noreply.github.com>
2026-06-26 17:48:46 +0000

    Point Vite entry to TypeScript app component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './sistema_de_gest_o_de_fluxo_ubs'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
